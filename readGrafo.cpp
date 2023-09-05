b0a9c19ef3126c0c0933302f9745dcbfa12bca81
nataliaqmc <nataliaqmc@al.insper.edu.br>
2023-09-05 10:19:32 -0300

    adding files

---
+++ readGrafo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+
+// Função para ler o grafo a partir do arquivo de entrada
+std::vector<std::vector<int>> LerGrafo(const std::string& nomeArquivo, int& numVertices) {
+    std::ifstream arquivo(nomeArquivo);
+    int numArestas;
+    arquivo >> numVertices >> numArestas;
+
+    std::vector<std::vector<int>> grafo(numVertices, std::vector<int>(numVertices, 0));
+
+    for (int i = 0; i < numArestas; ++i) {
+        int u, v;
+        arquivo >> u >> v;
+        grafo[u - 1][v - 1] = 1;
+        grafo[v - 1][u - 1] = 1;  // O grafo é não direcionado
+    }
+
+    arquivo.close();
+
+    return grafo;
+}
